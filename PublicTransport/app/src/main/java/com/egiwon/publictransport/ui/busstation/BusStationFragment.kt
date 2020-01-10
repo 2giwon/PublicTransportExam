bf03608d9bb8d9cd2665f7dcd556b8fd3e35d278
middlefgr <2giwon@gmail.com>
2020-01-11 00:37:28 +0900

    #01 리모트 데이터 가져온 값 셋팅 처리

--- PublicTransport/app/src/main/java/com/egiwon/publictransport/ui/busstation/BusStationFragment.kt
+++ PublicTransport/app/src/main/java/com/egiwon/publictransport/ui/busstation/BusStationFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
 import com.egiwon.publictransport.R
 import com.egiwon.publictransport.data.StationCallback
 import com.egiwon.publictransport.data.response.ServiceResult
@@ -11,27 +12,69 @@ import com.egiwon.publictransport.data.service.StationSearchService
 import kotlinx.android.synthetic.main.fragment_busstation.*
 
 class BusStationFragment : Fragment(R.layout.fragment_busstation), StationCallback {
+
+    private val onClick: (String) -> Unit = {
+
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         iv_search.setOnClickListener {
-            StationSearchService(
-                this
-            ).getStationInfo(et_search.text.toString(), serviceKey)
+            showProgressBar()
+            if (et_search.text.isNullOrBlank()) {
+                errorSearchStationEmpty()
+            } else {
+                StationSearchService(
+                    this
+                ).getStationInfo(et_search.text.toString(), serviceKey)
+            }
         }
+
+        with(rv_station) {
+            adapter = BusStationAdapter(onClick)
+            setHasFixedSize(true)
+            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+        }
+
     }
 
     override fun onSuccess(stationInfos: ServiceResult) {
-        Toast.makeText(requireContext(), "success", Toast.LENGTH_SHORT).show()
+        (rv_station.adapter as? BusStationAdapter)?.setItems(stationInfos)
+        hideProgressBar()
     }
 
     override fun onFailure(throwable: Throwable) {
+        errorloadStationFail(throwable)
+    }
+
+    private fun showProgressBar() {
+        progress_circular.visibility = View.VISIBLE
+    }
+
+    private fun hideProgressBar() {
+        progress_circular.visibility = View.GONE
+    }
+
+    private fun errorloadStationFail(throwable: Throwable) {
         Toast.makeText(
             requireContext(),
             requireContext().getString(R.string.error_load_station)
                     + throwable.localizedMessage,
             Toast.LENGTH_SHORT
         ).show()
+
+        hideProgressBar()
+    }
+
+    private fun errorSearchStationEmpty() {
+        Toast.makeText(
+            requireContext(),
+            getString(R.string.error_empty_station_name),
+            Toast.LENGTH_SHORT
+        ).show()
+
+        hideProgressBar()
     }
 
     companion object {
