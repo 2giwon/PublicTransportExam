9e0a8dd37ae97c545034153b6d7f1e2a47575af6
2giwon <2giwon@gmail.com>
2020-01-13 18:45:01 +0900

    #01 favorite fragment 구성 적용 및 adapter 구현

--- PublicTransport/app/src/main/java/com/egiwon/publictransport/ui/favorite/FavoriteFragment.kt
+++ PublicTransport/app/src/main/java/com/egiwon/publictransport/ui/favorite/FavoriteFragment.kt
@@ -1,6 +1,26 @@
 package com.egiwon.publictransport.ui.favorite
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import com.egiwon.publictransport.MainActivity
 import com.egiwon.publictransport.R
+import kotlinx.android.synthetic.main.fragment_favorite.*
 
-class FavoriteFragment : Fragment(R.layout.fragment_favorite)
+class FavoriteFragment : Fragment(R.layout.fragment_favorite) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(rv_favorite_station) {
+            adapter = FavoriteAdapter()
+            setHasFixedSize(true)
+            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+            (adapter as? FavoriteAdapter)?.run {
+                setItems((requireActivity() as MainActivity).favouriteList.toList())
+            }
+        }
+
+    }
+}
