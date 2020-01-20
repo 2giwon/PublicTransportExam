ffd185e6c424e15a89c8ad7b96401fc020d301b7
2giwon <2giwon@gmail.com>
2020-01-20 15:34:28 +0900

    #02 Retrofit object 변경

--- PublicTransport/app/src/main/java/com/egiwon/publictransport/data/service/StationSearchService.kt
+++ PublicTransport/app/src/main/java/com/egiwon/publictransport/data/service/StationSearchService.kt
@@ -1,21 +1,13 @@
 package com.egiwon.publictransport.data.service
 
 import com.egiwon.publictransport.BuildConfig
-import com.egiwon.publictransport.data.StationCallback
-import com.egiwon.publictransport.data.StationService
-import com.egiwon.publictransport.data.response.ServiceResult
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.simplexml.SimpleXmlConverterFactory
 
-class StationSearchService(
-    val receiver: StationCallback
-) {
-    private val retrofit: StationService = Retrofit.Builder()
+object StationSearchService {
+    val retrofit: StationService = Retrofit.Builder()
         .baseUrl("http://ws.bus.go.kr/api/rest/")
         .client(
             OkHttpClient.Builder()
@@ -33,36 +25,5 @@ class StationSearchService(
         .addConverterFactory(SimpleXmlConverterFactory.create())
         .build()
         .create(StationService::class.java)
-
-
-    fun getStationInfo(stationName: String, serviceKey: String) {
-        try {
-            retrofit.getStationInfo(
-                serviceKey,
-                stationName
-            ).enqueue(object : Callback<ServiceResult> {
-                override fun onFailure(call: Call<ServiceResult>, t: Throwable) =
-                    receiver.onFailure(t)
-
-                override fun onResponse(
-                    call: Call<ServiceResult>,
-                    response: Response<ServiceResult>
-                ) {
-                    response.body()?.let { serviceResult ->
-                        serviceResult.msgBody?.let { msgBody ->
-                            if (msgBody.itemList.isNullOrEmpty()) {
-                                receiver.onFailure(Throwable())
-                            } else {
-                                receiver.onSuccess(msgBody.itemList.toList())
-                            }
-
-                        }
-                    }
-                }
-            })
-        } catch (e: BootstrapMethodError) {
-            receiver.onFailure(Throwable())
-        }
-    }
 
 }
