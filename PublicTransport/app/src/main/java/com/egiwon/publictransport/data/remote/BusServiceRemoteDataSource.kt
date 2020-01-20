2c52a7f59cdcf6bec2dc5045f50daa477ad00480
2giwon <2giwon@gmail.com>
2020-01-20 15:33:41 +0900

    #02 dataSource 추가

---
+++ PublicTransport/app/src/main/java/com/egiwon/publictransport/data/remote/BusServiceRemoteDataSource.kt
@@ -0,0 +1,30 @@
+package com.egiwon.publictransport.data.remote
+
+import com.egiwon.publictransport.data.BusServiceDataSource
+import com.egiwon.publictransport.data.response.Item
+import com.egiwon.publictransport.data.service.StationSearchService.retrofit
+import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
+
+class BusServiceRemoteDataSource : BusServiceDataSource {
+    override fun getRemoteBusStationInfo(
+        stationName: String
+    ): Single<List<Item>> =
+        retrofit.getStationInfo(serviceKey = SERVICE_KEY, stationName = stationName)
+            .map { it.msgBody?.itemList?.toList() ?: emptyList() }
+            .observeOn(Schedulers.io())
+
+
+    companion object {
+        private const val SERVICE_KEY =
+            "zdPk2xTv930O2l4zxAjyh%2BCZeZKCY3UYhKsoFTlOfAvhCcEt0DdPZFxfQFsDrQgWLcTQHPunYimxI9WnTm3PFQ%3D%3D"
+
+
+        private var instance: BusServiceRemoteDataSource? = null
+
+        fun getInstance(): BusServiceRemoteDataSource =
+            instance ?: BusServiceRemoteDataSource().apply {
+                instance = this
+            }
+    }
+}
