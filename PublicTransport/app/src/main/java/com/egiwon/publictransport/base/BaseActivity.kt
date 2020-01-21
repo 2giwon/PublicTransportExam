35cb973eb421ae2f3adf28a8ab42ec49e04ca748
2giwon <2giwon@gmail.com>
2020-01-21 17:43:12 +0900

    #02 BaseActivity의 Presenter 작업 추가

--- PublicTransport/app/src/main/java/com/egiwon/publictransport/base/BaseActivity.kt
+++ PublicTransport/app/src/main/java/com/egiwon/publictransport/base/BaseActivity.kt
@@ -3,6 +3,14 @@ package com.egiwon.publictransport.base
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 
-abstract class BaseActivity(
+abstract class BaseActivity<P : BaseContract.Presenter>(
     @LayoutRes layoutResId: Int
-) : AppCompatActivity(layoutResId)
+) : AppCompatActivity(layoutResId) {
+
+    protected abstract val mainPresenter: P
+
+    override fun onDestroy() {
+        mainPresenter.clearDisposables()
+        super.onDestroy()
+    }
+}
