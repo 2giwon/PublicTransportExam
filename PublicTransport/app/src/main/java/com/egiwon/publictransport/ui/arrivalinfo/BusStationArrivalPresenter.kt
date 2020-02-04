0f556978771935a55a11e00ce508005e03386827
2giwon <2giwon@gmail.com>
2020-02-04 12:11:13 +0900

    #03 presenter에 버스 도착 정보 리스트를 갖고 있도록 수정

--- PublicTransport/app/src/main/java/com/egiwon/publictransport/ui/arrivalinfo/BusStationArrivalPresenter.kt
+++ PublicTransport/app/src/main/java/com/egiwon/publictransport/ui/arrivalinfo/BusStationArrivalPresenter.kt
@@ -2,6 +2,7 @@ package com.egiwon.publictransport.ui.arrivalinfo
 
 import com.egiwon.publictransport.base.BasePresenter
 import com.egiwon.publictransport.data.BusServiceRepository
+import com.egiwon.publictransport.data.response.ArrivalInfoItem
 import io.reactivex.android.schedulers.AndroidSchedulers
 
 class BusStationArrivalPresenter(
@@ -9,14 +10,22 @@ class BusStationArrivalPresenter(
     private val repository: BusServiceRepository
 ) : BasePresenter(), BusStationArrivalContract.Presenter {
 
+    private val arrivalInfoList = mutableListOf<ArrivalInfoItem>()
+
     override fun getBusStationArrivalInfo(arsId: String) {
         repository.getBusStationArrivalInfo(arsId)
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
-                view.showBusStationArrivalInfo(it)
+                arrivalInfoList.setItems(it)
+                view.showBusStationArrivalInfo(arrivalInfoList)
             }, {
                 view.showLoadFail(it)
             }).addDisposable()
+    }
+
+    private fun MutableList<ArrivalInfoItem>.setItems(items: List<ArrivalInfoItem>) {
+        clear()
+        addAll(items)
     }
 
 }
